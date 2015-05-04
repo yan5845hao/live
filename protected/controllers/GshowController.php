10b07526d63554c7d8a20a8f92e31d22ed56c511
tujunoo <tujunoo@qq.com>
2015-05-04 20:27:58 +0800

    uplod and gshow

---
+++ protected/controllers/GshowController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class GshowController
+ * @author toto tujunoo@qq.com
+ */
+class GshowController extends BaseController
+{
+	public function actionIndex()
+    {
+        $this->render('index');
+    }
+}
+
+
+?>
