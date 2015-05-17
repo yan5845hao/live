e75a339a505f556c7e34f5d9a5c165999c998899
Demi Mo <demi.mo@toursforfun.cn>
2015-05-17 14:08:12 +0800

    明星个人资料

--- protected/controllers/MyAccountController.php
+++ protected/controllers/MyAccountController.php
@@ -86,8 +86,43 @@ class MyAccountController extends BaseController
     }
 
     /**
-     * 以下是明星管理
+     * 以下是明星管理相关
      */
+    public function ActionEditStar()
+    {
+        $customer_id = Yii::app()->request->getParam('customer_id');
+        $file = $_FILES['faces'];
+        try{
+            for ($i = 0; $i < count($file['tmp_name']); $i++) {
+                if (!$file['tmp_name'][$i]) continue;
+                $content = fopen($file['tmp_name'][$i], 'r');
+                $extName = Yii::app()->aliyun->getExtName($file['name'][$i]);
+                $key = Yii::app()->aliyun->savePath . '/' . md5_file($file['tmp_name'][$i]) . '.' . $extName;
+                $size = $file['size'][$i];
+                Yii::app()->aliyun->putResourceObject($key, $content, $size);
+                $_POST['relation_star'][$i]['face'] = Yii::app()->params['cdnUrl'] . '/' . $key;
+            }
+            $relation_star = CJSON::encode($_POST['relation_star']);
+            $customer = Customer::model()->findByPk($customer_id);
+            $customerInfo = CustomerInfo::model()->findByPk($customer_id);
+            $customer->face = Yii::app()->request->getParam('face');
+            $customerInfo->content = Yii::app()->request->getParam('content');
+            $customerInfo->birthday = Yii::app()->request->getParam('birthday');
+            $customerInfo->address1 = Yii::app()->request->getParam('address1');
+            $customerInfo->height = Yii::app()->request->getParam('height');
+            $customerInfo->weight = Yii::app()->request->getParam('weight');
+            $customerInfo->occupation = Yii::app()->request->getParam('occupation');
+            $customerInfo->tag = Yii::app()->request->getParam('tag');
+            $customerInfo->relation_star = $relation_star;
+            $customer->save();
+            $customerInfo->save();
+            $this->redirect($this->createUrl('/myAccount'));
+        }catch(Exception $e){
+            $this->redirect($this->createUrl('/myAccount'));
+            $e->getMessage();
+            return false;
+        }
+    }
     public function actionNews()
     {
         $this->render('star/store');
--- protected/views/MyAccount/star/index.php
+++ protected/views/MyAccount/star/index.php
@@ -2,65 +2,78 @@
     <div class="userlj">当前位置：<a href="<?php echo Yii::app()->createUrl('/myAccount')?>">用户中心</a> > 我的资料</div>
     <div class="usercon">
         <?php include 'leftMenu.php' ;?>
+        <form action="<?php echo Yii::app()->createUrl('/myAccount/editStar');?>" method="post" id="form1" enctype="multipart/form-data">
         <div class="usercon981 right">
             <div class="usercon981tit">明星信息</div>
-            <div class="usercon981titline"><img src="css/img/userline.png" width="939" height="2" /></div>
+            <div class="usercon981titline"><img src="/css/img/userline.png" width="939" height="2" /></div>
             <div class="usercon981info">
                 <div style="font-size:14px; font-weight:bolder; margin-bottom:5px;">明星海报</div>
                 <div class="left">
-                    <?php $face = $userInfo->face?$userInfo->face:cdn_bumeng_url() . 'webserver/css/img/userline.png'; ?>
+                    <?php $face = $userInfo->face?$userInfo->face:cdn_bumeng_url() . 'webserver//css/img/userline.png'; ?>
                     <div>
                         <div id="imgshow" style="margin-bottom: -6px;"><img src="<?php echo $face;?>" width="205" height="205" /></div>
-                        <div style="line-height: 30px; height: 30px; font-size: 14px; background: #ccc;"><?php $this->widget('application.widgets.Upload.UploadWidget');?></div>
+                        <div style="line-height: 30px; height: 30px; font-size: 14px; background: #f0f0f0;"><?php $this->widget('application.widgets.Upload.UploadWidget');?></div>
                         <div id="imgError" style="color: red;"></div>
                     </div>
                 </div>
+                <input type="hidden" name="face" value="<?php echo $userInfo->face;?>" >
             </div>
             <div class="vspace" style="height:30px;"></div>
             <div class="usercon981con"><span>简介</span></div>
             <div class="usercon981con mxinfo">
-                <?php echo $userInfo->description->content;?>
+                <textarea name="content" style="width: 600px; height: 64px; resize:none; "><?php echo $userInfo->description->content;?></textarea>
             </div>
-            <div class="usercon981con"><span>生日</span><input name="" value="<?php echo $userInfo->description->birthday;?>" type="text"/><em></em></div>
-            <div class="usercon981con"><span>出生地</span><input name="" value="<?php echo $userInfo->description->address1;?>" type="text" /></div>
-            <div class="usercon981con"><span>身高</span><input name="" value="<?php echo $userInfo->description->height;?>" type="text" /></div>
-            <div class="usercon981con"><span>体重</span><input name="" value="<?php echo $userInfo->description->weight;?>" type="text" /></div>
-            <div class="usercon981con"><span>职业</span><input name="" value="<?php echo $userInfo->description->occupation;?>" type="text" /></div>
-            <div class="usercon981con"><span>他的词条</span><input name="" value="<?php echo $userInfo->description->tag;?>" type="text" /><em>请使用，分隔，否则不能显示</em></div>
-            <?php
-            $relation_star = CJSON::decode($userInfo->description->relation_star);
-//            print_vars($relation_star);
-            ?>
+            <input type="hidden" name="content" value="<?php echo $userInfo->description->content;?>" >
+            <div class="usercon981con"><span>生日</span><input name="birthday" value="<?php echo $userInfo->description->birthday;?>" type="text"/><em style="color: #ccc;">请填写日期格式，如：1990-04-20</em></div>
+            <div class="usercon981con"><span>出生地</span><input name="address1" value="<?php echo $userInfo->description->address1;?>" type="text" /></div>
+            <div class="usercon981con"><span>身高</span><input name="height" value="<?php echo $userInfo->description->height;?>" type="text" /></div>
+            <div class="usercon981con"><span>体重</span><input name="weight" value="<?php echo $userInfo->description->weight;?>" type="text" /></div>
+            <div class="usercon981con"><span>职业</span><input name="occupation" value="<?php echo $userInfo->description->occupation;?>" type="text" /></div>
+            <div class="usercon981con"><span>他的词条</span><input name="tag" value="<?php echo $userInfo->description->tag;?>" type="text" /><em style="color: #ccc;">请使用（,）分隔，如：高富帅,歌手</em></div>
+            <?php $relation_star = CJSON::decode($userInfo->description->relation_star); ?>
             <div class="mxcenter_tit">
-                <div class="usercon981con"><img src="css/img/mxcenter_line.png" width="941" height="6" /></div>
+                <div class="usercon981con"><img src="/css/img/mxcenter_line.png" width="941" height="6" /></div>
                 <div class="usercon981con" style="font-size:22px;">相关明星1</div>
-                <div class="usercon981con"><span>姓名</span><input name="name" value="" type="text" /></div>
-                <div class="usercon981con"><span>图片</span><input name="face" type="text" />
-                    <a href="#"><img src="css/img/mxcenter_btn01.jpg" width="89" height="40" style="margin-bottom:-16px;"/></a></div>
-                <div class="usercon981con"><img src="css/img/mxcenter_pic01.jpg" width="134" height="134" /></div>
-                <div class="usercon981con"><span>链接地址</span><input name="" type="text" /></div>
+                <div class="usercon981con"><span>姓名</span><input name="relation_star[0][name]" value="<?php echo $relation_star[0]['name']?>" type="text" /></div>
+                <div class="usercon981con"><span>图片</span><input name="faces[]" type="file" /></div>
+                <div class="usercon981con">
+                    <?php if(isset($relation_star[0]['face']) && $relation_star[0]['face']){ ?>
+                    <img src="<?php echo $relation_star[0]['face'];?>" width="134" height="134" />
+                    <?php } ?>
+                    <input type="hidden" name="relation_star[0][face]" value="<?php echo $relation_star[0]['face'];?>">
+                </div>
+                <div class="usercon981con"><span>链接地址</span><input name="relation_star[0][link]" value="<?php echo $relation_star[0]['link']?>" type="text" /></div>
             </div>
             <div class="mxcenter_tit">
-                <div class="usercon981con"><img src="css/img/mxcenter_line.png" width="941" height="6" /></div>
+                <div class="usercon981con"><img src="/css/img/mxcenter_line.png" width="941" height="6" /></div>
                 <div class="usercon981con" style="font-size:22px;">相关明星2</div>
-                <div class="usercon981con"><span>姓名</span><input name="" type="text" /></div>
-                <div class="usercon981con"><span>图片</span><input name="" type="text" />
-                    <a href="#"><img src="css/img/mxcenter_btn01.jpg" width="89" height="40" style="margin-bottom:-16px;"/></a></div>
-                <div class="usercon981con"><img src="css/img/mxcenter_pic01.jpg" width="134" height="134" /></div>
-                <div class="usercon981con"><span>链接地址</span><input name="" type="text" /></div>
+                <div class="usercon981con"><span>姓名</span><input name="relation_star[1][name]" value="<?php echo $relation_star[1]['name']?>" type="text" /></div>
+                <div class="usercon981con"><span>图片</span><input name="faces[]" type="file" /></div>
+                <div class="usercon981con">
+                    <?php if(isset($relation_star[1]['face']) && $relation_star[1]['face']){ ?>
+                        <img src="<?php echo $relation_star[1]['face'];?>" width="134" height="134" />
+                    <?php } ?>
+                    <input type="hidden" name="relation_star[1][face]" value="<?php echo $relation_star[1]['face'];?>">
+                </div>
+                <div class="usercon981con"><span>链接地址</span><input name="relation_star[1][link]" value="<?php echo $relation_star[1]['link']?>" type="text" /></div>
             </div>
             <div class="mxcenter_tit">
-                <div class="usercon981con"><img src="css/img/mxcenter_line.png" width="941" height="6" /></div>
+                <div class="usercon981con"><img src="/css/img/mxcenter_line.png" width="941" height="6" /></div>
                 <div class="usercon981con" style="font-size:22px;">相关明星3</div>
-                <div class="usercon981con"><span>姓名</span><input name="" type="text" /></div>
-                <div class="usercon981con"><span>图片</span><input name="" type="text" />
-                    <a href="#"><img src="css/img/mxcenter_btn01.jpg" width="89" height="40" style="margin-bottom:-16px;"/></a></div>
-                <div class="usercon981con"><img src="css/img/mxcenter_pic01.jpg" width="134" height="134" /></div>
-                <div class="usercon981con"><span>链接地址</span><input name="" type="text" /></div>
+                <div class="usercon981con"><span>姓名</span><input name="relation_star[2][name]" value="<?php echo $relation_star[2]['name']?>" type="text" /></div>
+                <div class="usercon981con"><span>图片</span><input name="faces[]" type="file" /></div>
+                <div class="usercon981con">
+                    <?php if(isset($relation_star[2]['face']) && $relation_star[2]['face']){ ?>
+                        <img src="<?php echo $relation_star[2]['face'];?>" width="134" height="134" />
+                    <?php } ?>
+                    <input type="hidden" name="relation_star[2][face]" value="<?php echo $relation_star[2]['face']?>">
+                </div>
+                <div class="usercon981con"><span>链接地址</span><input name="relation_star[2][link]" type="text" value="<?php echo $relation_star[2]['link']?>" /></div>
             </div>
-
-            <div class="usercon981con"><a href="#"><img src="css/img/userbtn01.jpg" width="260" height="40" /></a></div>
+            <div class="usercon981con"><a href="javascript:;" onclick="javascript:$('#form1').submit();"><img src="/css/img/userbtn01.jpg" width="260" height="40" /></a></div>
+            <input type="hidden" name="customer_id" value="<?php echo $userInfo['customer_id'];?>"
             <div class="vspace" style="height:20px;"></div>
         </div>
+        </form>
     </div>
 </div>
